79ff1a0a1976f2ea58a213cd9a77275c05d732ee
ghlee <neo622@naver.com>
2025-04-03 16:25:47 +0900

    feat: renderElement.js & 기본과제테스트

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,4 +1,5 @@
 import { addEvent } from "./eventManager";
+import { normalizeVNode } from "./normalizeVNode";
 //virtualDOM -> realDOM
 export function createElement(vNode) {
   // 예외 처리
@@ -14,32 +15,33 @@ export function createElement(vNode) {
   // 배열 → DocumentFragment
   if (Array.isArray(vNode)) {
     const fragment = document.createDocumentFragment();
-    vNode.forEach((child) => {
-      fragment.appendChild(createElement(child)); // 재귀 호출
-    });
+    vNode
+      .map(normalizeVNode) // virtualDOM 객체를 받을 수 있도록 전부 Normalize 진행
+      .forEach((child) => fragment.appendChild(createElement(child)));
     return fragment;
   }
-
   // 일반 VNode 처리
   const $el = document.createElement(vNode.type);
-
   // 속성 처리
   updateAttributes($el, vNode.props);
-
   // 자식 처리
   (vNode.children || []).forEach((child) => {
     $el.appendChild(createElement(child));
   });
-
   return $el;
 }
 
-function updateAttributes($el, props = {}) {
+function updateAttributes($el, props) {
+  if (!props || typeof props !== "object") return; // undefined, null, boolean 값에 대한 방어
+
   Object.entries(props).forEach(([attr, val]) => {
     if (val == null || val === false) return; // null, undefined, false 건너뜀
-
-    if (attr === "className") {
-      $el.setAttribute("class", val); // JSX → HTML 속성 변환
+    if (attr.startsWith("on") && typeof val === "function") {
+      // 이벤트 속성 처리 onClick > click > 위임 방식으로 처리
+      const eventType = attr.slice(2).toLowerCase();
+      addEvent($el, eventType, val);
+    } else if (attr === "className") {
+      $el.setAttribute("class", val); // JSX > HTML 속성 변환
     } else {
       $el.setAttribute(attr, val);
     }
--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,4 +1,4 @@
-import { setupEventListeners } from "./eventManager";
+import { setupEventListeners, clearEventMap } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
@@ -7,4 +7,13 @@ export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  clearEventMap(container)
+  console.log("vnode", vNode);
+  const normalizedNode = normalizeVNode(vNode);
+  console.log("정규화", normalizedNode);
+  const element = createElement(normalizedNode);
+  console.log("el", element);
+  container.innerHtml = "";
+  container.appendChild(element);
+  setupEventListeners(container);
 }
